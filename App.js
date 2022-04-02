d3c928bf4d7c7188d8b1ebc0662d5eff70754dc8
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-04-02 19:17:13 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+
+    //return <BottomTabNavigator>;
+    //<BottomTabNavigator/>
+    return <BottomTabNavigator />;
+    //return <BottomTabNavigator <BottomTabNavigator> />;
+
+  }
+}
---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import RideScreen from "../screens/Ride";
+import RideHistoryScreen from "../screens/RideHistory";
+
+const Tab = createBottomTabNavigator();
+
+export default class BottomTabNavigator extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Alugue uma Bicicleta" component={RideScreen} />
+
+          <Tab.Screen name="Histórico de Locações" component={RideHistoryScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
